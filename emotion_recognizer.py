4749153b4204dba23ef5028eadcef24d85d90068
yair-hb <aythb@hotmail.com>
2022-02-03 15:10:45 -0600

    se hacen algunas correcciones en el codigo

--- emotion_recognizer.py
+++ emotion_recognizer.py
@@ -4,6 +4,7 @@ import cv2
 import os
 from cv2 import resize 
 import numpy as np
+import imutils as im 
 
 def emotionImage(emotion):
     #emoji
@@ -31,7 +32,7 @@ imagePath = os.listdir(dataPath)
 print ('imagenPaths: ',imagePath)
 
 captura = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalFace_default.xml')
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 
 while True:
     ret, frame = captura.read()
@@ -48,36 +49,37 @@ while True:
         rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
         resultado = emotion_recognizer.predict(rostro)
 
-        cv2.putText(frame, '{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-
+        cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
+        
         if method == 'EigenFaces':
             if resultado[1] < 5700:
-                cv2.putText(frame, '{}'.format(imagePath[resultado[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+                cv2.putText(frame,'{}'.format(imagePath[resultado[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
                 cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
                 image = emotionImage(imagePath[resultado[0]])
-                nframe = cv2.hconcat([frame, image])
+                image2 = im.resize(image, width =480, height=300)
+                nframe = cv2.hconcat([frame, image2])
             else:
-                cv2.putText(frame, 'No identificado', (x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+                cv2.putText(frame,'No identificado',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
                 cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
                 nframe = cv2.hconcat([frame,np.zeros((480,300,3),dtype=np.uint8)])
         '''
         if method == 'FisherFace':
             if resultado[1] <500:
-                cv2.putText(frame,'{}'.format(imagePath[resultado[0]]),(x,y-25),2,1.1(0,255,0),1,cv2.LINE_AA)
+                cv2.putText(frame,'{}'.format(imagePath[resultado[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
                 cv2.rectangle(frame, (x,y),(x+y,y+h),(0,255,0),2)
                 image = emotionImage(imagePath[resultado[0]])
                 nframe = cv2.hconcat([frame, image])
             else: 
-                cv2.putText(frame,'No identificado', (x,y-20),2,0.8,(0,0,255),1, cv2.LINE_AA)
+                cv2.putText(frame,'No identificado',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
                 cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
                 nframe = cv2.hconcat([frame,np.zeros((480,300,3),dtype=np.uint8)])
         
         if method == 'LBPH':
             if resultado[1] <60:
-                cv2.putText(frame, '{}'.format(imagePath[resultado[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+                cv2.putText(frame,'{}'.format(imagePath[resultado[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
                 cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
                 image = emotionImage(imagePath[resultado[0]])
-                nframe = cv2.hconcat([frame,image])
+                nframe = cv2.hconcat([frame, image])
             else:
                 cv2.putText(frame,'No identificado',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
                 cv2.rectangle(frame, (x,y),(x+y,y+h),(0,0,255),2)
